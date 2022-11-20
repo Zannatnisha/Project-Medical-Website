1ab365977f99def4d5b2d28b2179f9823206b5b4
Fatema-121 <107356658+Fatema-121@users.noreply.github.com>
2022-11-20 21:56:28 +0600

    first commit

---
+++ basic-js/hello.js
@@ -0,0 +1,58 @@
+console.log(22);
+console.log('Good morning');
+
+var seenAfter=12;
+console.log(typeof seenAfter);
+var bananaPrice=30;
+console.log(typeof bananaPrice);
+
+var name= "Lorem Zuckerburg";
+console.log(typeof name);
+
+var isPoor=true;
+var isRich=false;
+console.log(typeof isPoor);
+
+var promise=" I promise I will work hard to become a web dveloper.";
+console.log(promise.toUpperCase());
+var promise=" I promise I will work HARD to become a WEB dveloper.";
+console.log(promise.toLowerCase());
+
+// Finding word position
+var promise=" I promise I will work hard to become a web dveloper.";
+console.log(promise.indexOf('will'));
+
+// devide all words
+var promise=" I promise I will work hard to become a web dveloper.";
+console.log(promise.split(' '));
+
+//adding
+var number1=12;
+var number2=20;
+console.log(number1 + number2);
+
+var number1=12.5;
+var number2=20;
+//number1=parseFloat(number1);
+//number1=parseInt(number1);
+console.log(number1 + number2);
+
+//convert to string
+var number1=12;
+var number2=20.5;
+number1= ''+(number1);
+console.log(typeof number1);
+
+//convert to number
+var number1=12;
+var number2=20.5;
+console.log(typeof number1);
+
+
+var number1=0.1;
+var number2=0.2;
+var total= number1 + number2;
+var total= total.toFixed (3); //doshomiker por jotogla songkha dekhte caibo totogulai bracket er moddhe likhte hobe
+console.log(total);
+
+
