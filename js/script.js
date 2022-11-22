019e25ff78080364f247ee9ed07a2a547e9f449b
Fatema-121 <107356658+Fatema-121@users.noreply.github.com>
2022-11-23 00:09:27 +0600

    js 2

--- js/script.js
+++ js/script.js
@@ -10,3 +10,21 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+let valuedisplays = document.querySelectorAll(".num");
+console.log(valuedisplays);
+let interval = 4000;
+
+valuedisplays.forEach((valuedisplay) => {
+    let startvalue = 0;
+    let endvalue = parseInt(valuedisplay.getAttribute("data-val"));
+    console.log(endvalue);
+    let duration = Math.floor(interval/endvalue);
+    let counter = setInterval(function() {
+        startvalue += 1;
+        valuedisplay.textContent = startvalue;
+        if (startvalue == endvalue) {
+            clearInterval(counter);
+        }       
+    }, duration);
+});
